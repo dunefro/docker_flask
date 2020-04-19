6fb143a3111bf815ca5c671cba13b9c786bd71c3
Vedant Pareek <dunefro@gmail.com>
2020-04-19 17:36:13 +0000

    Added list volumes function

--- helpers/docker_helper.py
+++ helpers/docker_helper.py
@@ -6,5 +6,9 @@ def list_containers():
     list_of_containers = client.containers.list()
     return list_of_containers
 
+def list_volumes():
+    list_of_volumes = client.volumes.list()
+    return list_of_volumes
+
 if __name__=='__main__':
     list_containers()
