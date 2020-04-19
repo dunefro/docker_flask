def77ac254a0034c95878a8697facd3a0c303f80
Vedant Pareek <dunefro@gmail.com>
2020-04-19 17:42:48 +0000

    added .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask 
-from helpers.docker_helper import list_containers
+from helpers.docker_helper import list_containers , list_volumes
 app = Flask(__name__) 
 print(__name__)
 
@@ -15,5 +15,16 @@ def list_docker_containers():
     for container in list_of_containers:
         response += '{} ----> {}\n'.format(container.short_id,container.name)
     return response
+
+@app.route('/list/volumes',methods=['GET'])
+def list_docker_volumes():
+    list_of_volumes = list_volumes()
+    response = ''
+    for volume in list_of_volumes:
+        response += '{}'.format(volume.short_id)
+    if response:
+        return response
+    else:
+        return ' No Volumes created yet'
 
 app.run()
