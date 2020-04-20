bbd85c5ffe5f7f0b2ebbe973a9359208adc7e056
Vedant Pareek <dunefro@gmail.com>
2020-04-20 18:02:25 +0000

    docker_compose version 1

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from flask import Flask 
 from helpers.docker_helper import list_containers , list_volumes
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 app = Flask(__name__) 
 print(__name__)
-
+log_template = '------------------------- {} ----------------------------------'
 @app.route('/')
 def home_page():
-    return 'Docker based web application'
+    return 'Docker based web application\n'
 
 @app.route('/list/containers',methods=['GET'])
 def list_docker_containers():
 
+    logging.info(log_template.format('Listing Containers') + '\n')
     list_of_containers = list_containers()
     response = ''
     for container in list_of_containers:
