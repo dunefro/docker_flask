a003e0ad8bb3a687b64793ff6634b1a1ac72ebed
Vedant Pareek <dunefro@gmail.com>
2020-04-21 18:01:42 +0000

    Added helper section for listing images

--- helpers/docker_helper.py
+++ helpers/docker_helper.py
@@ -10,5 +10,9 @@ def list_volumes():
     list_of_volumes = client.volumes.list()
     return list_of_volumes
 
+def list_images():
+    list_of_images = [ img for img in client.images.list() if img.tags ]
+    return list_of_images
+
 if __name__=='__main__':
     list_containers()
