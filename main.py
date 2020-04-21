37cc6d76663376c56275daf9604e75d9af37782d
Vedant Pareek <dunefro@gmail.com>
2020-04-21 18:12:27 +0000

    Added different objects in route call

--- main.py
+++ main.py
@@ -11,41 +11,56 @@ log_template = '------------------------- {} ----------------------------------'
 def home_page():
     return 'Docker based web application\n'
 
-@app.route('/list/containers',methods=['GET'])
-def list_docker_containers():
+# @app.route('/list/containers',methods=['GET'])
+# def list_docker_containers():
 
-    logging.info(log_template.format('Listing Containers') + '\n')
-    list_of_containers = list_containers()
-    response = ''
-    for container in list_of_containers:
-        response += '{} ----> {}\n'.format(container.short_id,container.name)
-    if response:
-        return response
-    else:
-        return 'No containers created yet'
+#     logging.info(log_template.format('Listing Containers') + '\n')
+#     list_of_containers = list_containers()
+#     response = ''
+#     for container in list_of_containers:
+#         response += '{} ----> {}\n'.format(container.short_id,container.name)
+#     if response:
+#         return response
+#     else:
+#         return 'No containers created yet'
 
-@app.route('/list/volumes',methods=['GET'])
-def list_docker_volumes():
-    list_of_volumes = list_volumes()
-    response = ''
-    for volume in list_of_volumes:
-        response += '{}\n'.format(volume.short_id)
-    if response:
-        return response
-    else:
-        return 'No Volumes created yet'
+# @app.route('/list/volumes',methods=['GET'])
+# def list_docker_volumes():
+#     list_of_volumes = list_volumes()
+#     response = ''
+#     for volume in list_of_volumes:
+#         response += '{}\n'.format(volume.short_id)
+#     if response:
+#         return response
+#     else:
+#         return 'No Volumes created yet'
 
 @app.route('/list/<string:name>',methods=['GET'])
 def list_objects(name):
-    if name == "images":
-        list_of_obj = list_images()
+    
+    logging.info(log_template.format('Listing {}'.format(name)) + '\n')
     response = ''
-    for obj in list_of_obj:
-        # if obj.tags:
-        response += '{}------------{}\n'.format(obj.short_id,obj.tags)
+    
+    if name == 'images':
+        list_of_obj = list_images()
+        for obj in list_of_obj:
+            response += '{}------------{}\n'.format(obj.short_id,obj.tags)
+
+    elif name == 'volumes':
+        list_of_obj = list_volumes()
+        for volume in list_of_volumes:
+            response += '{}\n'.format(volume.short_id)
+
+    elif name == 'containers':
+        for container in list_of_containers:
+            response += '{} ----> {}\n'.format(container.short_id,container.name)
+            
+    else:
+        return 'No resource named {}'.format(name)
+
     if response:
         return response
     else:
-        return "Not {} created yet".format(name)
+        return 'No {} created yet'.format(name)
 
 app.run(host='0.0.0.0',port=5000)
